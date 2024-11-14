173aded2b3b2b87026bd1548a9d2ae932cb71e70
Geraldo <geraldo.morais@gmail.com>
2024-11-14 16:56:45 -0300

    correção de importação

--- app/upload/FileUpload.tsx
+++ app/upload/FileUpload.tsx
@@ -1,4 +1,5 @@
-import { Button, Input, Label } from "@/components/ui"; // Reverting to the original import path
+import { Input } from 'components/ui/input'
+import { Label } from 'components/ui/label'
 import { useState } from "react";
 
 interface FileUploadProps {
