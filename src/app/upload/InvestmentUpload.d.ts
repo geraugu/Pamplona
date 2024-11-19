88d8c22c19b1ff2d7b07a50a219d1c0c83aa1892
Geraldo <geraldo.morais@gmail.com>
2024-11-19 14:25:40 -0300

    teste

--- src/app/upload/InvestmentUpload.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-interface InvestmentUploadProps {
-  onBack: () => void
-  onUploadInvestments: (csvFile: File, investmentDate: string) => void
-}
-
-export declare function InvestmentUpload(props: InvestmentUploadProps): React.ReactElement
